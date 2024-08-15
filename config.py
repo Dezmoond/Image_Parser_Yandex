07246d2daceefc623bcd7776a506db8afe7e3495
Dezmoond <85880988+Dezmoond@users.noreply.github.com>
2024-08-16 00:03:24 +0900

    Add files via upload

--- config.py
+++ config.py
@@ -7,3 +7,4 @@ CLIENT_ID = 'c610f40746a3ef8' #Imgur key
 private_key = 'private_zR+5BqvveuLQFy/e7BFzeeuEKs4='
 public_key = 'public_VqDqLF7IsKe9pDGWYDgGY3wjCI8='
 
+resize_option = 0 #0 дорисовывать края / 1 растягивать изображение
